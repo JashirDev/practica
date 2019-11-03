b447341bbcb4c46a70a7b32af59cedf3ef21f983
jashir <jashirchirre@gmail.com>
2019-11-02 22:17:07 -0500

    ejecucion
    EL proyecto parece funcionar pero falta probar las apis

--- mongopractica/src/main/java/com/example/demo/MongopracticeApplication.java
+++ mongopractica/src/main/java/com/example/demo/MongopracticeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MongopracticeApplication {
-
+//ejecuta mongod.exe antes de iniciar el cliente
 	public static void main(String[] args) {
 		SpringApplication.run(MongopracticeApplication.class, args);
 	}
--- mongopractica/src/main/java/com/example/demo/model/Employee.java
+++ mongopractica/src/main/java/com/example/demo/model/Employee.java
@@ -4,17 +4,23 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
 @ToString
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
-public class Employee {
+@Document(collection = "Employee")
+public class Employee implements Serializable {
 
     @Id
+    @NotNull
     private String id;
+
     private String name;
+
     private Double salary;
 
 }
